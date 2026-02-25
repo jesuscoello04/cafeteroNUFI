6b35f977c8467b867951e85b791c72a00aaedb15
Bytestian76 <sebas.urenasilva@gmail.com>
2026-02-25 11:06:16 -0500

    Configuradas variables de entorno, App Factory en 'app/__init__' y config.py

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__, template_folder='views')
+    app.config.from_object('config.Config')
+
+    # Inicializar extensiones
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Debes iniciar sesión para acceder.'
+
+    # Registrar blueprints (controladores)
+    from app.controllers.auth_controller import auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
